3788d18a23437ab4d605dab685ee2c5a5b720107
Samuel Stock <samuelstock@Samuels-MBP.lan>
2024-06-21 16:45:05 +0100

    adding additional examples for soc embedding example

--- notebooks/soc_embedding_example.py
+++ notebooks/soc_embedding_example.py
@@ -19,11 +19,28 @@ input_data = [
         "job_title": "Anaesthetist",
         "company": "London General Hospital",
     },
+    {
+        "id": "3",
+        "job_title": "Operational Researcher",
+        "company": "Department for Environment, Food and Rural Affairs",
+    },
 ]
 
 result = embed.search_index(
-    input_data=input_data, id_field="id", process_output=True
+    input_data=input_data,
+    id_field="id",
+    embedded_fields=["job_title", "company"],
+    process_output=True,
 )
 
-print(result)
+result
+
+result = embed.search_index(
+    input_data=input_data,
+    id_field="id",
+    embedded_fields=["job_title"],
+    process_output=True,
+)
+
+result
 # %%
