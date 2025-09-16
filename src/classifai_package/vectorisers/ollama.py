34d8e97c65047016126d7264675cd661a96a669c
Thomas Owen <thomas.owen@ons.gov.uk>
2025-09-16 16:39:27 +0100

    refactor(dependencies): move optional-dep using vectorisers into modules

---
+++ src/classifai_package/vectorisers/ollama.py
@@ -0,0 +1,50 @@
+"""A module for embedding text using a locally-running Ollama server."""
+
+import numpy as np
+
+from classifai_package._optional import check_deps
+
+from .base import VectoriserBase
+
+
+class OllamaVectoriser(VectoriserBase):
+    """A wrapper class allowing a locally-running ollama server to generate text embeddings.
+
+    Attributes:
+        model_name (str): The name of the local model to use.
+    """
+
+    def __init__(self, model_name: str):
+        """Initializes the OllamaVectoriser with the specified model name and device.
+
+        Args:
+            model_name (str): The name of the local model to use.
+
+        Notes:
+            requires an ollama server to be running locally (`ollama serve`)
+        """
+        check_deps(["ollama"], extra="ollama")
+        self.model_name = model_name
+
+    def transform(self, texts):
+        """Transforms input text(s) into embeddings using the Huggingface model.
+
+        Args:
+            texts (str or list of str): The input text(s) to embed. Can be a single string or a list of strings.
+
+        Returns:
+            numpy.ndarray: A 2D array of embeddings, where each row corresponds to an input text.
+
+        Raises:
+            TypeError: If the input is not a string or a list of strings.
+        """
+        import ollama  # type: ignore
+
+        if isinstance(texts, str):
+            texts = [texts]
+
+        if not isinstance(texts, list):
+            raise TypeError("Input must be a string or a list of strings.")
+
+        response = ollama.embed(model=self.model_name, input=texts)
+        return np.array(response.embeddings)
