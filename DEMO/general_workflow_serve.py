55cf58d1d9be3497452234cc23d23cb774845375
rileyok-ons <205916635+rileyok-ons@users.noreply.github.com>
2025-11-07 16:45:17 +0000

    feat(demo): added api demo script

---
+++ DEMO/general_workflow_serve.py
@@ -0,0 +1,23 @@
+##### first we load the vectoriser used in the vectorstore creation
+from classifai.indexers import VectorStore
+from classifai.servers import start_api
+from classifai.vectorisers import HuggingFaceVectoriser
+
+# Our embedding model is pulled down from HuggingFace, or used straight away if previously downloaded
+# This also works with many different huggingface models!
+vectoriser = HuggingFaceVectoriser(model_name="sentence-transformers/all-MiniLM-L6-v2")
+
+
+#### now we can load the vectorstore back in without having to create it again
+loaded_vectorstore = VectorStore.from_filespace("./DEMO/testdata", vectoriser)
+
+
+#### look wow! you can search it straight away cause it was laoded back in
+
+print(f"Test search {loaded_vectorstore.search('did the quick brown fox jump over the log?')}")
+
+
+#### and finally, its easy to search your vectorstore via a restAPI service, just run:
+start_api([loaded_vectorstore], endpoint_names=["my_vectorstore"])
+
+# Look at https://0.0.0.0:8000/docs to see the Swagger API documentation and test in the browser
