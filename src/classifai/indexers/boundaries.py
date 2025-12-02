eb319a1a4d76ac484de840f3bf72ec6fb28aa922
frayle-ons <194791647+frayle-ons@users.noreply.github.com>
2025-12-02 17:24:19 +0000

    pandera bug fixing for vectorstore search return validation

--- src/classifai/indexers/boundaries.py
+++ src/classifai/indexers/boundaries.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 from typing import Literal
 
-import pandera as pa
+import pandera.pandas as pa
 from pydantic import BaseModel, DirectoryPath, Field, FilePath, field_validator, model_validator
 from typing_extensions import Self
 
@@ -11,7 +11,7 @@ from ..vectorisers.base import VectoriserBase
 #######
 
 
-class VectorStoreInput:
+class VectorStoreInput(BaseModel):
     file_name: FilePath = Field(
         description="Path to the data file to be indexed.",
     )
@@ -30,7 +30,10 @@ class VectorStoreInput:
     )
     overwrite: bool = Field(False, description="Whether to overwrite existing vector store in the output directory.")
 
-    @field_validator("meta_data", model="after")
+    class Config:
+        arbitrary_types_allowed = True
+
+    @field_validator("meta_data", mode="after")
     def validate_metadata(cls, value):
         if value is None:
             return {}
@@ -43,10 +46,13 @@ class FromFileSpaceInput(BaseModel):
     )  # DirectoryPath ensures the path is a valid directory on the filesystem compared to Path used in the VectorStoreInput above which doesn't need to exist
     vectoriser: VectoriserBase = Field(description="An instance of a class inheriting from VectoriserBase.")
 
+    class Config:
+        arbitrary_types_allowed = True
+
 
 class SearchInput(BaseModel):
     query: str | list[str] = Field(..., description="The text query or list of queries to search for.")
-    ids: list[str | int] = Field(
+    ids: list[str | int] | None = Field(
         None,
         description="List of query IDs. Must be unique and match the length of the query list.",
     )
@@ -67,6 +73,7 @@ class SearchInput(BaseModel):
 
         if self.ids is None:
             self.ids = [list(range(len(self.query)))]
+            self.ids = [str(i) for i in self.ids]
 
         elif len(set(self.ids)) != len(self.ids):
             raise ValueError("'ids' must contain unique values.")
@@ -77,13 +84,13 @@ class SearchInput(BaseModel):
         return self
 
 
-class SearchOutputSchema(pa.SchemaModel):
-    query_id: pa.typing.Series[str]  # Ensure query_id is a string
-    query_text: pa.typing.Series[str]  # Ensure query_text is a string
-    doc_id: pa.typing.Series[str]  # Ensure doc_id is a string
-    doc_text: pa.typing.Series[str]  # Ensure doc_text is a string
-    rank: pa.typing.Series[int] = pa.Field(ge=0)  # Ensure rank is a non-negative integer
-    score: pa.typing.Series[float] = pa.Field(ge=0.0, le=1.0)  # Ensure score is between 0 and 1
+class SearchOutputSchema(pa.DataFrameModel):
+    query_id: str  # Ensure query_id is a string
+    query_text: str  # Ensure query_text is a string
+    doc_id: str  # Ensure doc_id is a string
+    doc_text: str  # Ensure doc_text is a string
+    rank: int = pa.Field(ge=0)  # Ensure rank is a non-negative integer
+    score: float = pa.Field(ge=0.0)  # Ensure score is between 0 and 1
 
     # Add metadata columns dynamically if needed
     class Config:
@@ -92,7 +99,7 @@ class SearchOutputSchema(pa.SchemaModel):
 
 class ReverseSearchInput(BaseModel):
     query: str | list[str] = Field(description="The text query or list of queries to search for.")
-    ids: list[str | int] = Field(
+    ids: list[str | int] | None = Field(
         None,
         description="List of query IDs. Must be unique and match the length of the query list.",
     )
@@ -109,7 +116,8 @@ class ReverseSearchInput(BaseModel):
             self.query = [self.query]
 
         if self.ids is None:
-            self.ids = [list(range(len(self.query)))]
+            self.ids = list(range(len(self.query)))
+            self.ids = [str(i) for i in self.ids]
 
         elif len(set(self.ids)) != len(self.ids):
             raise ValueError("'ids' must contain unique values.")
@@ -120,12 +128,10 @@ class ReverseSearchInput(BaseModel):
         return self
 
 
-class ReverseSearchOutputSchema(pa.SchemaModel):
-    query_id: pa.typing.Series[str]  # Ensure query_id is a string
-    doc_id: pa.typing.Series[str]  # Ensure doc_id is a string
-    doc_text: pa.typing.Series[str]  # Ensure doc_text is a string
-    rank: pa.typing.Series[int] = pa.Field(ge=0)  # Ensure rank is a non-negative integer
-    score: pa.typing.Series[float] = pa.Field(ge=0.0, le=1.0)  # Ensure score is between 0 and 1
+class ReverseSearchOutputSchema(pa.DataFrameModel):
+    query_id: str  # Ensure query_id is a string
+    doc_id: str  # Ensure doc_id is a string
+    doc_text: str  # Ensure doc_text is a string
 
     # Add metadata columns dynamically if needed
     class Config:
