756e4b630aac4d1785291de1d7337ff898c57341
Luke <206716137+lukeroantreeONS@users.noreply.github.com>
2026-03-27 12:40:30 +0000

    chore(RAGHook): create config_generator as an attribute to localise all genai definitions to class __init__ method

--- src/classifai/indexers/hooks/default_hooks/postprocessing.py
+++ src/classifai/indexers/hooks/default_hooks/postprocessing.py
@@ -4,9 +4,8 @@ from collections.abc import Callable
 
 import numpy as np
 import pandas as pd
-from classifai._optional import check_deps
-#from google import genai
 
+from classifai._optional import check_deps
 from classifai.exceptions import ConfigurationError, HookError
 from classifai.indexers.dataclasses import VectorStoreSearchOutput
 from classifai.indexers.hooks.hook_factory import HookBase
@@ -130,6 +129,7 @@ class RagHook(HookBase):
 
         try:
             self.client = genai.Client(**self.client_kwargs)  # .aio
+            self.config_generator = genai.types.GenerateContentConfig  # type: ignore
         except Exception as e:
             raise ConfigurationError(
                 "Failed to initialize GCP GenAI client.",
@@ -234,7 +234,7 @@ class RagHook(HookBase):
             response = self.client.models.generate_content(  # await ...
                 model=self.model_name,
                 contents=prompt,
-                config=genai.types.GenerateContentConfig(system_instruction=self.context_prompt),
+                config=self.config_generator(system_instruction=self.context_prompt),
             )
             updated_search_output.loc[search_subset.index, "RAG_response"] = self.llm_response_parser(
                 search_subset, response.text
