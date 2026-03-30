4d9da357fdfd5a46cdec7c232de9c3836d70c998
rileyok-ons <205916635+rileyok-ons@users.noreply.github.com>
2026-03-30 11:06:09 +0000

    chore: updated hook docstring

--- src/classifai/indexers/hooks/__init__.py
+++ src/classifai/indexers/hooks/__init__.py
@@ -1,4 +1,4 @@
-"""Hooks submodule contains factory for hooks and example prebuilt hooks for users."""
+"""This module contains a factory for, and example pre-built hooks. Allowing users to manipulate the content of ClassifAI dataclass objects as they enter or leave the `VectorStore`."""
 
 from .default_hooks import CapitalisationStandardisingHook, DeduplicationHook, RagHook
 from .hook_factory import HookBase
