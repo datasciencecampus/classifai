f3e36655c0bee632e0932902ac842839708c9755
rileyok-ons <205916635+rileyok-ons@users.noreply.github.com>
2026-03-30 10:11:47 +0000

    docs(hooks): added hooks to docs

--- src/classifai/indexers/hooks/__init__.py
+++ src/classifai/indexers/hooks/__init__.py
@@ -1,3 +1,5 @@
+"""Hooks submodule contains factory for hooks and example prebuilt hooks for users."""
+
 from .default_hooks import CapitalisationStandardisingHook, DeduplicationHook, RagHook
 from .hook_factory import HookBase
 
