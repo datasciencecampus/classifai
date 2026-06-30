5783a7316fe6ece344a44a738fef82c9e0162a21
Jamie Milsom <jamie.milsom@ons.gov.uk>
2026-06-30 16:55:42 +0100

    update VectorStore batch_size to 128 from 250

--- src/classifai/indexers/main.py
+++ src/classifai/indexers/main.py
@@ -83,7 +83,7 @@ class VectorStore:
         file_name: str,
         data_type: str,
         vectoriser: VectoriserBase,
-        batch_size: int = 250,
+        batch_size: int = 128,
         meta_data: dict | None = None,
         output_dir: str | None = None,
         overwrite: bool = False,
@@ -100,7 +100,7 @@ class VectorStore:
             vectoriser (object): The `Vectoriser` object used to transform text into
                                  vector embeddings.
             batch_size (int): [optional] The batch size for processing the input file and batching to
-            vectoriser. Defaults to 250.
+            vectoriser. Defaults to 128.
             meta_data (dict): [optional] key,value pair metadata column names to extract from the input file and their types.
                                 Defaults to `None`.
             output_dir (str): [optional] The directory where the `VectorStore` will be saved.
